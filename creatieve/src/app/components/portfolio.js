053d9a553286da7882b078b0a5bed08b998f3474
Александр <nikol.alex06@mail.ru>
2025-01-03 14:44:39 +0300

    feat: Added portfolio block (desktop)

--- creatieve/src/app/components/portfolio.js
+++ creatieve/src/app/components/portfolio.js
@@ -1,26 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
 let works = [
     {
         id: 1,
         title: 'OlimpEducation',
         desc: 'Это веб-приложение помогает школьникам готовиться к олимпиада, предоставляя задания по определенным выбранным критериям.',
-        urlImgDesk: '',
-        urlImgMob: '',
+        urlImgDesk: '/Desktop - 1.jpg',
+        urlImgMob: '/Android Large - 1.jpg',
         isReverse: false
     },
     {
         id: 2,
         title: 'VTB.fines',
         desc: 'Это приложение наша команда разрабатывала как конкурентное преимущество банка ВТБ. В нем можно осуществлять платежи, отслеживать свой рейтинг и прочее.',
-        urlImgDesk: '',
-        urlImgMob: '',
+        urlImgDesk: '/Desktop - Profile page.jpg',
+        urlImgMob: '/Profile page.jpg',
         isReverse: true
     },
     {
         id: 3,
         title: 'Neotechnica',
         desc: 'Этот кооперативный сайт находится в процессе разработки на смену устаревшей версии сайт с сохранением стилистики, цветов и логотипов компании',
-        urlImgDesk: '',
-        urlImgMob: '',
+        urlImgDesk: '/Main page 2 variant.jpg',
+        urlImgMob: '/Android Large - 1.jpg',
         isReverse: false
     }
 ]
@@ -33,7 +34,7 @@ export default function Portfolio() {
                     <h3 className="text-white font-base text-4xl font-bold"><span className="text-bright">О качестве</span> наши <span>работы</span> говорят за нас</h3>
                     <p className="text-2xl text-white">представляем вам несколько наших работ</p>
                 </div>
-                <ul>
+                <ul className="flex flex-col gap-y-14">
                     {works.map(work => <Work key={work.id} work={work}/>)}
                 </ul>
             </div>
@@ -43,8 +44,19 @@ export default function Portfolio() {
 
 function Work({work}) {
     return (
-        <li className={`flex ${work.isReverse && 'flex-row-reverse'}`}>
-            
+        <li className={`flex ${work.isReverse && 'flex-row-reverse'} items-center w-full`}>
+            <div className="w-1/2 flex gap-5">
+                <div className="w-[492px] h-[350px] overflow-y-scroll rounded-[10px]">
+                    <img alt={work.title} className="w-full h-auto" src={work.urlImgDesk}></img>
+                </div>
+                <div className="w-[161px] h-[350px] overflow-y-scroll rounded-[10px]">
+                    <img alt={work.title} className="w-full h-auto" src={work.urlImgMob}></img>
+                </div>
+            </div>
+            <div className={`flex flex-col gap-7 ${work.isReverse ? 'pr-10' : 'pl-10'}`}>
+                <h5 className="font-base text-white text-2xl font-bold">{work.title}</h5>
+                <p className="text-white text-2xl">{work.desc}</p>
+            </div>
         </li>
     )
 }
